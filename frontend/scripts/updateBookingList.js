f24018363cff13bcdf2e07e7251653e2d6acf9ca
jizhouli <JustinLi.ljt@gmail.com>
2015-05-17 10:39:29 +0800

    添加异步数据保存;添加数据队列;日志输出到console

--- frontend/scripts/updateBookingList.js
+++ frontend/scripts/updateBookingList.js
@@ -1,24 +1,33 @@
 call_count = 1
-function updateBookingList() {
-	var request = getHTTPObject();
-	if (request) {
+function updateBookingList(successHandler, errorHandler) {
+	var xhr = getHTTPObject();
+	if (xhr) {
 		// weather info - provided by www.openweathermap.org
-		request.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Beijing,cn", true)
+		xhr.open("GET", "http://api.openweathermap.org/data/2.5/weather?q=Beijing,cn", true)
 		
-		request.onreadystatechange = function() {
-			if (request.readyState == 4) {
-				jsonObject = JSON.parse(request.responseText);
-				weather = jsonObject['weather'][0]['main'];
+		xhr.onreadystatechange = function() {
+			if (xhr.readyState == 4) {
+				status = xhr.status;
+				if (status == 200) {
+					data = JSON.parse(xhr.responseText);
+					weather = data['weather'][0]['main'];
 
-				var para = document.createElement("p");
-				var txt = document.createTextNode(call_count + ' : ' + weather);
-				para.appendChild(txt);
-				document.getElementById("footer").appendChild(para);
-				call_count += 1;
+					var para = document.createElement("p");
+					var txt = document.createTextNode(call_count + ' : ' + weather);
+					para.appendChild(txt);
+					document.getElementById("footer").appendChild(para);
+					call_count += 1;
+
+					successHandler && successHandler([call_count + ' : ' + weather]);				
+				}
+				else {
+					errorHandler && errorHandler(status);
+				}
 			}
 		};
-		request.send(null);
+		xhr.send(null);
 	} else {
-		alert("Your browser doesn\'t support XMLHttpRequest");
+		errorHandler && errorHandler(status);
+		alert("Your browser doesn\'t support XMLHttpxhr");
 	}
 }
